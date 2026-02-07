aed5739d27fdd0f856f9bdaff90b521498ecd58e
Vatsal Sanjay <vatsalsanjay@gmail.com>
2026-02-07 00:03:34 +0000

    temp files in giignore

--- basilisk/src/darcsit/record.sh
+++
@@ -1,9 +0,0 @@
-# If the recorded patch adds or removes files, the Makefile.tests in
-# the corresponding directories need to be re-generated
-for f in `darcs changes --last=1 -v |        \
-    grep -E '^    (add|rm)file ' | \
-    sed -E 's/^    (add|rm)file //g'`; do
-    dir=`darcs show repo | grep Root | awk '{print $2}'`/`dirname $f`
-    rm -f $dir/Makefile.tests $dir/Makefile.deps $dir/*.d
-done
-darcs push -a -q wiki@shell.basilisk.fr:wiki
